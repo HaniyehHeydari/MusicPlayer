3adcc8ae93e181346e7abe6d1c89e005ecf4135b
haniyeh <heydarihaniyeh51@gmail.com>
2024-06-03 19:59:20 +0330

    Add Login Page

---
+++ app/src/main/java/com/example/mobile/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
